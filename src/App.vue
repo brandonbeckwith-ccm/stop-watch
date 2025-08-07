0745f83931a108210c30add1573811f6c17f51ee
anmol-ccm <anmol.sharma@myccmortgage.com>
2025-08-07 20:23:32 +0530

    completed task of stopwatch

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
+import Stopwatch from './components/Stopwatch.vue';
 </script>
 
 <template>
   <div id="app">
     <h1>Stopwatch Assignment</h1>
-    <p>Build your stopwatch here! Check the README.md for requirements.</p>
+    <Stopwatch />
   </div>
 </template>
 
