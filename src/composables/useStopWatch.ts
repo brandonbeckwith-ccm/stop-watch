da61a5c6b1af195bbdface33d0d9527c7a6a7041
Sourabh Sawant <sourabh.sawant@v2solutions.com>
2025-08-07 21:13:22 +0530

    Added stopwatch feature

---
+++ src/composables/useStopWatch.ts
@@ -0,0 +1,53 @@
+import { ref, computed } from "vue"
+
+export type Lap = {
+    id: number,
+    timeString: string
+}
+
+export function useStopWatch() {
+    const time = ref<number>(0);
+    const interval = ref<number>(0);
+    const isRunning = ref<boolean>(false);
+    const laps = ref<Lap[]>([]);
+
+    const formatTime = computed<string>(() => {
+        const ms = (time.value % 1000).toString().padStart(2, '0').slice(0, 2);
+        const totalSeconds = Math.floor(time.value / 1000);
+        const seconds = (totalSeconds % 60).toString().padStart(2, '0');
+        const minutes = Math.floor(totalSeconds / 60).toString().padStart(2, '0');
+
+        return `${minutes} : ${seconds} : ${ms}`;
+    });
+
+    const start = (): void => {
+        isRunning.value = true;
+        interval.value = setInterval(() => time.value += 10, 10)
+    };
+
+    const stop = (): void => {
+        isRunning.value = false;
+        clearInterval(interval.value);
+    };
+
+    const reset = (): void => {
+        stop();
+        time.value = 0;
+        laps.value = [];
+    };
+
+    const lap = (): void => {
+        laps.value.push({ id: laps.value.length + 1, timeString: formatTime.value });
+    };
+
+    return {
+        formatTime,
+        laps,
+        isRunning,
+        interval,
+        start,
+        stop,
+        reset,
+        lap
+    }
+}
