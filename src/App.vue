3566b6b8ac18abc9e4ce5e3d13f9ba32071cf3c9
sweta-ccm <sweta.das@myccmortgage.com>
2025-08-07 19:03:18 +0530

    Resolve conflicts using rebase

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,11 @@
 <script setup lang="ts">
-import { CButton, CTag } from "@ccm-engineering/ui-components";
-import { useStopwatch } from "./composables/useStopWatch";
+import StopWatch from './custom-component/StopWatch.vue';
 
-const { start, stop, reset, lap, formattedTime, formattedLaps, running } =
-  useStopwatch();
 </script>
+
 <template>
   <div id="app">
-    <h1>Stopwatch Assignment</h1>
-
-    <div class="stopwatch">
-      <h1>{{ formattedTime }}</h1>
-
-      <div class="controls">
-        <CButton label="Start" v-if="!running" @click="start" />
-        <CButton label="Stop" v-if="running" @click="stop" />
-        <CButton label="Reset" @click="reset" />
-        <CButton label="Lap" :disabled="!running" @click="lap" />
-      </div>
-
-      <div class="laps" v-if="formattedLaps.length">
-        <h2>Laps</h2>
-        <ul>
-          <li v-for="(lap, index) in formattedLaps" :key="index">
-            <CTag :label="`Lap ${index + 1}: ${lap}`" />
-          </li>
-        </ul>
-      </div>
-    </div>
+   <StopWatch></StopWatch>
   </div>
 </template>
 
